a173441406b47bbb05cdaabca7766325d525a44a
KommandNyang <baejungmin077@gmail.com>
2022-02-01 12:55:03 +0900

    Revert "Update docusaurus.config.js"
    This reverts commit e9102ebd22c70c791de157c2b6efa227f55b83e7.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 module.exports = {
   title: 'Proper Crew Docs',
   tagline: '',
-  url: 'https://docs.propercrew.org/',
+  url: 'https://docs.projecttl.net/',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -101,7 +101,7 @@ module.exports = {
           routeBasePath: '/',
           // Please change this to your repo.
           editUrl:
-            'https://github.com/PROJECT-OPERATOR/propercrew-docs/tree/master',
+            'https://github.com/PROJECT-OPERATOR/propercrew-docs',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
